b05df81801be55c3e775ded0a0b9cfbdecb801a4
AKDevOpps <86369262+AKDevOpps@users.noreply.github.com>
2022-11-03 02:38:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner ')
+
+
+steamlit.header('Breakfast menu')
+steamlit.text('Omega 3 & Blueberry Oatmeal ')
+steamlit.text('Kale, Spinach & Rocket Smoothie')
+steamit.text('Hard-Boiled Free Range Egg')
